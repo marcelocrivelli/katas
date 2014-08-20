461f5c1b92bca893521880822dd5fa3f59d4e367
marcelocrivelli <crivellimarcelo@gmail.com>
2014-08-20 18:45:21 -0300

    nil-key
    nil-key

--- code-katas-1/src/code_katas_1/core.clj
+++ code-katas-1/src/code_katas_1/core.clj
@@ -10,7 +10,13 @@
 (defn nil-key
   "Escribir una funcion que dada una clave y un mapa, devuelva true, solamente si el mapa
    contiene una entrada con esa clave, y su valor es nil"
-  [k m])
+  [k m]
+  (if (contains? m k)
+    (if (nil? (get m k))
+      true
+      false)
+    false  )
+  )
 
 (defn range
   "Escribir una funcion que cree una lista de enteros en un rango dado.
